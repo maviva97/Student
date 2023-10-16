3f8dfda5f17f2efe581eca4d02af7d8d5fe95550
maviva97@gmail.com <maviva97@gmail.com>
2023-10-16 11:54:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Student someStudent= new Student("Ion","Botanica","IT",2023,3500.00);
+
+        System.out.println(someStudent);
+    }
+}
